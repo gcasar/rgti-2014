49e9db7b03a07fc4f59f133face7ef1e26ac6255
Gasper Bavdek <gasperbavdek@Gaspers-MacBook-Pro.local>
2015-02-24 14:50:53 +0100

    Working physic on server

--- app/js/terminal.js
+++ app/js/terminal.js
@@ -15,6 +15,7 @@ var config = {
       exports: 'Backbone'
     },
     'stats': { exports: 'Stats' },
+    'pixi': {exports: 'PIXI'},
     // --- Use shim to mix together all THREE.js subcomponents
     'threeCore': { exports: 'THREE' },
     'BinaryLoader': { deps: ['threeCore'], exports: 'THREE' },
@@ -26,6 +27,7 @@ var config = {
     backbone: 'lib/backbone',
     box2d: 'lib/box2d',
     gamejs: 'lib/gamejs.min',
+    pixi: 'lib/pixi',
 
     three: 'lib/three',
     threeCore: 'lib/three.min',
@@ -43,13 +45,16 @@ require.config(config);
 
 require([
 	'config',
-	'jquery'
-], function( CONFIG, $) {
+	'jquery',
+  'pixi'
+], function( CONFIG, $,PIXI) {
 	var cbck = null;
 	var socket = null;
   var circles=[];
+  var KEYS_DOWN = [];
+  var ACTION = {};
 
-	connect();
+	//connect();
 
 	function connect(_cbck){
       socket = new WebSocket('ws://'+CONFIG.SERVER_URL+':'+CONFIG.SERVER_PORT+'/');
@@ -80,7 +85,7 @@ require([
           cbck("connected");
         }else if(payload.action==="dif"){
           circles=payload.data;
-          drawCircles(circles);
+          //drawCircles(circles);
         }else{
           console.log("Error, bad type");
         }
@@ -103,21 +108,114 @@ require([
       }
     }
 
-    function drawCircles(circles){
-      var canvas = $("#canvas")[0];
-      var ctx = canvas.getContext("2d");
+    //drawing elements
+    var canvas = $("#canvas")[0];
+    var ctx = canvas.getContext("2d");
 
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      for(var k in circles){
-        var centerX = circles[k][0];
-        var centerY = circles[k][1];
-        var radius= 35;
+    function drawCircle(x,y,radius){
         ctx.beginPath();
-        ctx.fillStyle = "#600000";
-        ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-        ctx.closePath();
+        ctx.fillStyle = "#FE8E9D";
         ctx.fill();
+        ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+        ctx.stroke();
+    }
+    function drawSquare(x,y,angle,width,height){
+      ctx.translate(x,y);
+      ctx.rotate(angle);
+      ctx.fillStyle = 'blue';
+      ctx.fillRect(width / -2, height / -2, width, height);
+      ctx.rotate(-angle);
+      ctx.translate(-x,-y);
+    }
 
+    //control car from terminal
+
+    $(document).keydown(function(e){
+          switch(e.which){
+            case 37: // left
+            KEYS_DOWN['left'] = true;
+            break;
+
+            case 38: // up
+            KEYS_DOWN['up'] = true;
+            break;
+
+            case 39: // right
+            KEYS_DOWN['right'] = true;
+            break;
+
+            case 40: // down
+
+            KEYS_DOWN['down'] = true;
+            break;
+
+            default: return; // exit this handler for other keys
+        }
+        parsekeys();
+        e.preventDefault(); // prevent the default action (scroll / move caret)
+    });
+
+       $(document).keyup(function(e){
+
+          switch(e.which){
+            case 37: // left
+            KEYS_DOWN['left'] = false;
+            break;
+
+            case 38: // up
+
+            KEYS_DOWN['up'] = false;
+            break;
+
+            case 39: // right
+            KEYS_DOWN['right'] = false;
+            break;
+
+            case 40: // down
+
+            KEYS_DOWN['down'] = false;
+            break;
+
+            default: return; // exit this handler for other keys
+        }
+        parsekeys();
+        e.preventDefault(); // prevent the default action (scroll / move caret)
+       });
+  
+    function parsekeys() {
+      if(KEYS_DOWN['up']&&!KEYS_DOWN['down']){
+        if(KEYS_DOWN['right']&&!KEYS_DOWN['left']){
+          console.log("action upright");
+          //remote.action("upright");
+        }else if(!KEYS_DOWN['right']&&KEYS_DOWN['left']){
+          console.log("action upleft")
+          //remote.action("upleft");
+        }else{
+          console.log("action up")
+          //remote.action("up");
+        }
+      }else if(!KEYS_DOWN['up']&&KEYS_DOWN['down']){
+        if(KEYS_DOWN['right']&&!KEYS_DOWN['left']){
+          console.log("action downright")
+          //remote.action("downright");
+        }else if(!KEYS_DOWN['right']&&KEYS_DOWN['left']){
+          console.log("action downleft")
+          //remote.action("downleft");
+        }else{
+          console.log("action down")
+          //remote.action("down");
+        }
+      }else{
+        if(KEYS_DOWN['right']&&!KEYS_DOWN['left']){
+          console.log("action stopright")
+          //remote.action("stopright");
+        }else if(!KEYS_DOWN['right']&&KEYS_DOWN['left']){
+          console.log("action stopleft")
+          //remote.action("stopleft");
+        }else{
+          console.log("action stop")
+          //remote.action("stop");
+        }
       }
     }
 
